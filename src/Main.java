d1d623730c765c887077f296dfba3152441cd1c9
Giovanna Midonz <midonz09@outlook.com>
2023-06-19 08:52:12 -0300

    Curso e Mentoria

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,30 @@
+import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Mentoria;
+
+import java.time.LocalDate;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Curso curso1 = new Curso();
+
+        curso1.setTitulo("programação");
+        curso1.setDescricao("curso para quem deseja desenvolver software");
+        curso1.setCargaHoraria(224);
+
+        Curso curso2 = new Curso();
+        curso2.setTitulo("Ondotondologia");
+        curso2.setDescricao("Se torne um dentista de primeira ");
+        curso2.setCargaHoraria(445);
+
+
+        Mentoria mentoria = new Mentoria();
+        mentoria.setTitulo("Mentoria para programacao");
+        mentoria.setDescricao("para dar suporte as suas duvidas");
+        mentoria.setData(LocalDate.now());
+
+        System.out.println(curso1);
+        System.out.println(curso2);
+        System.out.println(mentoria);
     }
 }
